bc7258613f6dc1f21a53433c4f32969d61a37099
davidklun <davidklun@f6d94a88-5fdb-4aa2-ad4d-abd8a21b5ff8>
2009-02-18 20:59:58 +0000

    no message

--- pipeline/Readme_pipeline.txt
+++ pipeline/Readme_pipeline.txt
@@ -1,15 +1,19 @@
 The verilog files, fpu_addsub.v and fpu_mul.v, are pipelined versions of
-floating point operators.  Rounding is not supported by these operators, and
-denormalized numbers are treated as 0.  If infinity or NaN is on either of the inputs,
-then infinity will be the output.  Both operators, addsub and mul, have a latency
-of 18 clock cycles, and then an output is available on each clock cycle after the latency.
+floating point operators.  The four rounding modes (Nearest, To Zero, To
+Positive Infintiy, To Negative Infinity) are supported by these operators. 
+Denormalized numbers are not supported, but instead are treated as 0.  
+If infinity or NaN is on either of the inputs,
+then infinity will be the output.  Addition and subtraction have a latency
+of 24 clock cycles, and then an output is available on each clock cycle after the latency.
+Multiplication has a latency of 21 clock cycles, and 
+then an output is available on each clock cycle after the latency.
 
 For addition and subtraction, fpu_addsub.v was synthesized with an estimated 
-frequency of 276 MHz for a Virtex5 device.  The synthesis results are below.  
+frequency of 259 MHz for a Virtex5 device.  The synthesis results are below.  
 The file, fpu_addsub_TB.v, is the testbench used to simulate fpu_addsub.v.
 
 For multiplication, fpu_mul.v was synthesized with an estimated 
-frequency of 426 MHz for a Virtex5 device.  The synthesis results are below.  
+frequency of 393 MHz for a Virtex5 device.  The synthesis results are below.  
 The file, fpu_mul_TB.v, is the testbench used to simulate fpu_mul.v. 
 
 Please email me any questions.
@@ -20,46 +24,47 @@ davidklun@gmail.com
 addsub synthesis results:
 
 ---------------------------------------
-Resource Usage Report for fpu 
+Resource Usage Report for fpu_addsub 
 
 Mapping to part: xc5vsx95tff1136-2
 Cell usage:
-FDE             16 uses
+FDE             55 uses
 FDR             6 uses
-FDRE            2350 uses
+FDRE            2848 uses
 GND             1 use
-MUXCY           7 uses
-MUXCY_L         183 uses
+MUXCY           8 uses
+MUXCY_L         293 uses
 VCC             1 use
-XORCY           130 uses
-XORCY_L         4 uses
-LUT1            14 uses
-LUT2            386 uses
-LUT3            448 uses
-LUT4            133 uses
-LUT5            103 uses
-LUT6            496 uses
+XORCY           240 uses
+XORCY_L         5 uses
+LUT1            98 uses
+LUT2            377 uses
+LUT3            522 uses
+LUT4            151 uses
+LUT5            101 uses
+LUT6            517 uses
 
-I/O ports: 197
-I/O primitives: 196
-IBUF           131 uses
+I/O ports: 199
+I/O primitives: 198
+IBUF           133 uses
 OBUF           65 uses
 
 BUFGP          1 use
 
 SRL primitives:
-SRL16E         16 uses
+SRLC32E        1 use
+SRL16E         54 uses
 
 I/O Register bits:                  0
-Register bits not including I/Os:   2372 (4%)
+Register bits not including I/Os:   2909 (4%)
 
 Global Clock Buffers: 1 of 32 (3%)
 
 Total load per clock:
-   fpu|clk: 2388
+   fpu_addsub|clk: 2964
 
 Mapping Summary:
-Total  LUTs: 1596 (2%)
+Total  LUTs: 1821 (3%)
 
 ------------------------------
 
@@ -71,40 +76,39 @@ Resource Usage Report for fpu_mul
 Mapping to part: xc5vsx95tff1136-2
 Cell usage:
 DSP48E          9 uses
-FDE             80 uses
-FDRE            1221 uses
+FDE             83 uses
+FDRE            1536 uses
 FDRSE           11 uses
 GND             1 use
-MUXCY           4 uses
-MUXCY_L         82 uses
+MUXCY           7 uses
+MUXCY_L         164 uses
 VCC             1 use
-XORCY           75 uses
-XORCY_L         3 uses
-LUT1            25 uses
-LUT2            203 uses
-LUT3            57 uses
-LUT4            30 uses
-LUT5            7 uses
-LUT6            14 uses
+XORCY           128 uses
+XORCY_L         5 uses
+LUT1            82 uses
+LUT2            215 uses
+LUT3            170 uses
+LUT4            48 uses
+LUT5            13 uses
+LUT6            32 uses
 
-I/O ports: 196
-I/O primitives: 195
-IBUF           130 uses
+I/O ports: 198
+I/O primitives: 197
+IBUF           132 uses
 OBUF           65 uses
 
 BUFGP          1 use
 
 SRL primitives:
-SRLC32E        1 use
-SRL16E         27 uses
+SRL16E         83 uses
 
 I/O Register bits:                  0
-Register bits not including I/Os:   1312 (2%)
+Register bits not including I/Os:   1630 (2%)
 
 Global Clock Buffers: 1 of 32 (3%)
 
 Total load per clock:
-   fpu_mul|clk: 1349
+   fpu_mul|clk: 1722
 
 Mapping Summary:
-Total  LUTs: 364 (0%)
+Total  LUTs: 643 (1%)
--- pipeline/fpu_mul.v
+++ pipeline/fpu_mul.v
@@ -32,21 +32,26 @@
 ////                                                             ////
 /////////////////////////////////////////////////////////////////////
 
+// rmode = 00 (nearest), 01 (to zero), 10 (+ infinity), 11 (- infinity)
+
 `timescale 1ns / 100ps
 
-module fpu_mul( clk, rst, enable, opa, opb, ready, outfp);
+module fpu_mul( clk, rst, enable, rmode, opa, opb, ready, outfp);
 input		clk;
 input		rst;
 input		enable;
+input	[1:0]	rmode;
 input	[63:0]	opa, opb;
 output		ready; 
 output  [63:0] outfp;
 
 
-
 reg  	product_shift;
-reg   sign, sign_1, sign_2, sign_3, sign_4, sign_5, sign_6, sign_7, sign_8;
-reg   sign_9, sign_10, sign_11, sign_12, sign_13, sign_14, sign_15, sign_16, sign_17;
+reg		[1:0] rm_1, rm_2, rm_3, rm_4, rm_5, rm_6, rm_7, rm_8, rm_9;
+reg		[1:0] rm_10, rm_11, rm_12, rm_13, rm_14, rm_15;
+reg   	sign, sign_1, sign_2, sign_3, sign_4, sign_5, sign_6, sign_7, sign_8;
+reg   	sign_9, sign_10, sign_11, sign_12, sign_13, sign_14, sign_15, sign_16, sign_17;
+reg	 	sign_18, sign_19, sign_20;
 reg   [51:0] mantissa_a1, mantissa_a2;
 reg   [51:0] mantissa_b1, mantissa_b2;
 reg   [10:0] exponent_a;
@@ -58,14 +63,13 @@ reg		in_zero_1;
 reg   [11:0] exponent_terms_1, exponent_terms_2, exponent_terms_3, exponent_terms_4;
 reg   [11:0] exponent_terms_5, exponent_terms_6, exponent_terms_7;
 reg   [11:0] exponent_terms_8, exponent_terms_9;
-reg    exponent_gt_expoffset;
+reg    	exponent_gt_expoffset;
 reg   [11:0] exponent_1;
 wire   [11:0] exponent = 0;
 reg   [11:0] exponent_2, exponent_2_0, exponent_2_1;
-reg   exponent_gt_prodshift, exponent_is_infinity, exponent_is_infinity_2;
-reg   [11:0] exponent_3;
-reg   [11:0] exponent_4;
-reg  set_mantissa_zero;
+reg   	exponent_gt_prodshift, exponent_is_infinity;
+reg   [11:0] exponent_3, exponent_4;
+reg  	set_mantissa_zero, set_mz_1;
 reg   [52:0] mul_a, mul_a1, mul_a2, mul_a3, mul_a4, mul_a5, mul_a6, mul_a7, mul_a8;
 reg   [52:0] mul_b, mul_b1, mul_b2, mul_b3, mul_b4, mul_b5, mul_b6, mul_b7, mul_b8;
 reg		[40:0] product_a;
@@ -95,13 +99,17 @@ reg		[29:0] sum_6;
 reg		[36:0] sum_7;
 reg		[16:0] sum_7_2;
 reg		[30:0] sum_8;
-reg   [105:0] product;
-reg   [105:0] product_1;
-reg   [105:0] product_2, product_3;
-reg		product_lsb; // if there are any 1's in the remainder
-reg  [55:0] product_4; 
-reg  [11:0] exponent_5, exponent_6;		
-wire  [63:0] outfp = { sign, exponent_6[10:0], product_4[53:2]};
+reg   	[105:0] product;
+reg   	[105:0] product_1;
+reg   	[52:0] product_2, product_3;
+reg  	[53:0] product_4, product_5, product_6, product_7; 
+reg		product_overflow;
+reg  	[11:0] exponent_5, exponent_6, exponent_7, exponent_8, exponent_9;		
+reg		round_nearest_mode, round_posinf_mode, round_neginf_mode;
+reg		round_nearest_trigger, round_nearest_exception;
+reg		round_nearest_enable, round_posinf_trigger, round_posinf_enable;
+reg		round_neginf_trigger, round_neginf_enable, round_enable;
+wire  	[63:0] outfp = { sign, exponent_9[10:0], product_7[51:0]};
 
 always @(posedge clk) 
 begin
@@ -109,90 +117,74 @@ begin
 		sign <= 0; sign_1 <= 0; sign_2 <= 0; sign_3 <= 0; sign_4 <= 0;
 		sign_5 <= 0; sign_6 <= 0; sign_7 <= 0; sign_8 <= 0; sign_9 <= 0;
 		sign_10 <= 0; sign_11 <= 0; sign_12 <= 0; sign_13 <= 0; 
-		sign_14 <= 0; sign_15 <= 0; sign_16 <= 0; sign_17 <= 0;
-		mantissa_a1 <= 0;
-		mantissa_b1 <= 0;
-		mantissa_a2 <= 0;
-		mantissa_b2 <= 0;
-		exponent_a <= 0;
-		exponent_b <= 0;
-		a_is_zero <= 0; b_is_zero <= 0; 
-		a_is_inf <= 0; b_is_inf <= 0; in_inf_1 <= 0; in_inf_2 <= 0;
-		in_zero_1 <= 0; 
-		exponent_terms_1 <= 0; exponent_terms_2 <= 0; exponent_terms_3 <= 0;
-		exponent_terms_4 <= 0; exponent_terms_5 <= 0; exponent_terms_6 <= 0;
-		exponent_terms_7 <= 0; exponent_terms_8 <= 0; exponent_terms_9 <= 0;
-		exponent_gt_expoffset <= 0;
-		exponent_1 <= 0; 
+		sign_14 <= 0; sign_15 <= 0; sign_16 <= 0; sign_17 <= 0; sign_18 <= 0; sign_19 <= 0; 
+		sign_20 <= 0; mantissa_a1 <= 0; mantissa_b1 <= 0; mantissa_a2 <= 0; mantissa_b2 <= 0;
+		exponent_a <= 0; exponent_b <= 0; rm_1 <= 0; rm_2 <= 0; rm_3 <= 0; rm_4 <= 0; rm_5 <= 0; 
+		rm_6 <= 0; rm_7 <= 0; rm_8 <= 0; rm_9 <= 0; rm_10 <= 0; rm_11 <= 0; 
+		rm_12 <= 0; rm_13 <= 0; rm_14 <= 0; rm_15 <= 0; 
+		a_is_zero <= 0; b_is_zero <= 0; a_is_inf <= 0; b_is_inf <= 0; in_inf_1 <= 0; in_inf_2 <= 0;
+		in_zero_1 <= 0; exponent_terms_1 <= 0; exponent_terms_2 <= 0; exponent_terms_3 <= 0;
+		exponent_terms_4 <= 0; exponent_terms_5 <= 0; exponent_terms_6 <= 0; exponent_terms_7 <= 0; 
+		exponent_terms_8 <= 0; exponent_terms_9 <= 0; exponent_gt_expoffset <= 0; exponent_1 <= 0; 
 		exponent_2_0 <= 0; exponent_2_1 <= 0; exponent_2 <= 0; exponent_gt_prodshift <= 0;
-		exponent_is_infinity <= 0; exponent_is_infinity_2 <= 0;
-		exponent_3 <= 0;
-		exponent_4 <= 0;
-		set_mantissa_zero <= 0;
-		mul_a <= 0; mul_b <= 0; mul_a1 <= 0; mul_b1 <= 0;  mul_a2 <= 0; mul_b2 <= 0;
-		mul_a3 <= 0; mul_b3 <= 0; mul_a4 <= 0; mul_b4 <= 0;  mul_a5 <= 0; mul_b5 <= 0;
-		mul_a6 <= 0; mul_b6 <= 0; mul_a7 <= 0; mul_b7 <= 0;  mul_a8 <= 0; mul_b8 <= 0;
+		exponent_is_infinity <= 0; exponent_3 <= 0; exponent_4 <= 0;
+		set_mantissa_zero <= 0; set_mz_1 <= 0; mul_a <= 0; mul_b <= 0; mul_a1 <= 0; mul_b1 <= 0;  
+		mul_a2 <= 0; mul_b2 <= 0; mul_a3 <= 0; mul_b3 <= 0; mul_a4 <= 0; mul_b4 <= 0;  mul_a5 <= 0; 
+		mul_b5 <= 0; mul_a6 <= 0; mul_b6 <= 0; mul_a7 <= 0; mul_b7 <= 0;  mul_a8 <= 0; mul_b8 <= 0;
 		product_a <= 0; product_a_2 <= 0; product_a_3 <= 0; product_a_4 <= 0; product_a_5 <= 0;
 		product_a_6 <= 0; product_a_7 <= 0; product_a_8 <= 0; product_a_9 <= 0; product_a_10 <= 0;
 		product_b <= 0; product_c <= 0; product_d <= 0; product_e <= 0; product_f <= 0;
 		product_g <= 0; product_h <= 0; product_i <= 0; product_j <= 0;
 		sum_0 <= 0; sum_0_2 <= 0; sum_0_3 <= 0; sum_0_4 <= 0; sum_0_5 <= 0; sum_0_6 <= 0; 
-		sum_0_7 <= 0; sum_0_8 <= 0; sum_0_9 <= 0;
-		sum_1 <= 0; sum_1_2 <= 0; sum_1_3 <= 0; sum_1_4 <= 0; sum_1_5 <= 0; sum_1_6 <= 0; 
-		sum_1_7 <= 0; sum_1_8 <= 0; 
-		sum_2 <= 0; sum_2_2 <= 0; sum_2_3 <= 0; sum_2_4 <= 0; sum_2_5 <= 0; sum_2_6 <= 0; sum_2_7 <= 0;
-		sum_3 <= 0; sum_4 <= 0; sum_4_2 <= 0; sum_4_3 <= 0; sum_4_4 <= 0; sum_4_5 <= 0;
-		sum_5 <= 0; sum_5_2 <= 0; sum_5_3 <= 0; sum_5_4 <= 0;
-		sum_6 <= 0; sum_7 <= 0; sum_7_2 <= 0; sum_8 <= 0;
-		product <= 0;
-		product_1 <= 0;
-		product_2 <= 0; product_3 <= 0; 
-		product_lsb <= 0;
-		exponent_5 <= 0; exponent_6 <= 0;
-		product_shift <= 0;
+		sum_0_7 <= 0; sum_0_8 <= 0; sum_0_9 <= 0; sum_1 <= 0; sum_1_2 <= 0; sum_1_3 <= 0; sum_1_4 <= 0; 
+		sum_1_5 <= 0; sum_1_6 <= 0; sum_1_7 <= 0; sum_1_8 <= 0; sum_2 <= 0; sum_2_2 <= 0; sum_2_3 <= 0; 
+		sum_2_4 <= 0; sum_2_5 <= 0; sum_2_6 <= 0; sum_2_7 <= 0; sum_3 <= 0; sum_4 <= 0; sum_4_2 <= 0; 
+		sum_4_3 <= 0; sum_4_4 <= 0; sum_4_5 <= 0; sum_5 <= 0; sum_5_2 <= 0; sum_5_3 <= 0; sum_5_4 <= 0;
+		sum_6 <= 0; sum_7 <= 0; sum_7_2 <= 0; sum_8 <= 0; product <= 0; product_1 <= 0; product_2 <= 0; 
+		product_3 <= 0; product_4 <= 0; product_5 <= 0; product_overflow <= 0; product_6 <= 0; 
+		exponent_5 <= 0; exponent_6 <= 0; exponent_7 <= 0; exponent_8 <= 0; product_shift <= 0;
+		product_7 <= 0; exponent_9 <= 0;
+		round_nearest_mode <= 0; round_posinf_mode <= 0; round_neginf_mode <= 0; round_nearest_trigger <= 0; 
+		round_nearest_exception <= 0; round_nearest_enable <= 0; round_posinf_trigger <= 0; round_posinf_enable <= 0;
+		round_neginf_trigger <= 0; round_neginf_enable <= 0; round_enable <= 0;
 	end
 	else if (enable) begin
 		sign_1 <= opa[63] ^ opb[63]; sign_2 <= sign_1; sign_3 <= sign_2; sign_4 <= sign_3;
 		sign_5 <= sign_4; sign_6 <= sign_5; sign_7 <= sign_6; sign_8 <= sign_7; sign_9 <= sign_8;
 		sign_10 <= sign_9; sign_11 <= sign_10; sign_12 <= sign_11; sign_13 <= sign_12;
-		sign_14 <= sign_13; sign_15 <= sign_14; sign_16 <= sign_15; sign_17 <= sign_16; sign <= sign_17;
-		mantissa_a1 <= opa[51:0];
-		mantissa_b1 <= opb[51:0];
-		mantissa_a2 <= mantissa_a1;
-		mantissa_b2 <= mantissa_b1;
-		exponent_a <= opa[62:52];
-		exponent_b <= opb[62:52];
-		a_is_zero <= !(|exponent_a);
-		b_is_zero <= !(|exponent_b);
-		a_is_inf <= exponent_a == 2047;
-		b_is_inf <= exponent_b == 2047;
-		in_inf_1 <= a_is_inf | b_is_inf;
-		in_inf_2 <= in_inf_1;
+		sign_14 <= sign_13; sign_15 <= sign_14; sign_16 <= sign_15; sign_17 <= sign_16; 
+		sign_18 <= sign_17; sign_19 <= sign_18; sign_20 <= sign_19; sign <= sign_20;
+		mantissa_a1 <= opa[51:0]; mantissa_b1 <= opb[51:0]; mantissa_a2 <= mantissa_a1;
+		mantissa_b2 <= mantissa_b1; exponent_a <= opa[62:52]; exponent_b <= opb[62:52];
+		rm_1 <= rmode; rm_2 <= rm_1; rm_3 <= rm_2; rm_4 <= rm_3; 
+		rm_5 <= rm_4; rm_6 <= rm_5; rm_7 <= rm_6; rm_8 <= rm_7; rm_9 <= rm_8; 
+		rm_10 <= rm_9; rm_11 <= rm_10; rm_12 <= rm_11; rm_13 <= rm_12; rm_14 <= rm_13;
+		rm_15 <= rm_14; 
+		a_is_zero <= !(|exponent_a); b_is_zero <= !(|exponent_b);
+		a_is_inf <= exponent_a == 2047; b_is_inf <= exponent_b == 2047;
+		in_inf_1 <= a_is_inf | b_is_inf; in_inf_2 <= in_inf_1;
 		in_zero_1 <= a_is_zero | b_is_zero;
 		exponent_terms_1 <= exponent_a + exponent_b;
 		exponent_terms_2 <= exponent_terms_1;
 		exponent_terms_3 <= in_zero_1 ? 12'b0 : exponent_terms_2;
 		exponent_terms_4 <= in_inf_2 ? 12'b110000000000 : exponent_terms_3;
-		exponent_terms_5 <= exponent_terms_4;
-		exponent_terms_6 <= exponent_terms_5;
-		exponent_terms_7 <= exponent_terms_6;
-		exponent_terms_8 <= exponent_terms_7;
+		exponent_terms_5 <= exponent_terms_4; exponent_terms_6 <= exponent_terms_5;
+		exponent_terms_7 <= exponent_terms_6; exponent_terms_8 <= exponent_terms_7;
 		exponent_terms_9 <= exponent_terms_8;
 		exponent_gt_expoffset <= exponent_terms_9 > 1022;
 		exponent_1 <= exponent_terms_9 - 1022; 
 		exponent_2_0 <= exponent_gt_expoffset ? exponent_1 : exponent;
 		exponent_2_1 <= exponent_2_0;
 		exponent_2 <= exponent_2_1; 
-		exponent_is_infinity <= exponent_2 > 2046;
-		exponent_is_infinity_2 <= exponent_is_infinity;
+		exponent_is_infinity <= (exponent_3 > 2046) & exponent_gt_prodshift;
 		exponent_3 <= exponent_2 - product_shift;
 		exponent_gt_prodshift <= exponent_2 >= product_shift;
 		exponent_4 <= exponent_gt_prodshift ? exponent_3 : exponent;
-		exponent_5 <= exponent_is_infinity_2 ? 12'b011111111111 : exponent_4;
-		set_mantissa_zero <= exponent_4 == 0 | exponent_is_infinity_2;
+		exponent_5 <= exponent_is_infinity ? 12'b011111111111 : exponent_4;
+		set_mantissa_zero <= exponent_4 == 0 | exponent_is_infinity;
+		set_mz_1 <= set_mantissa_zero;
 		exponent_6 <= exponent_5;
-		mul_a <= { !a_is_zero, mantissa_a2 };
-		mul_b <= { !b_is_zero, mantissa_b2 };
+		mul_a <= { !a_is_zero, mantissa_a2 }; mul_b <= { !b_is_zero, mantissa_b2 };
 		mul_a1 <= mul_a; mul_b1 <= mul_b;
 		mul_a2 <= mul_a1; mul_b2 <= mul_b1; mul_a3 <= mul_a2; mul_b3 <= mul_b2;
 		mul_a4 <= mul_a3; mul_b4 <= mul_b3; mul_a5 <= mul_a4; mul_b5 <= mul_b4;
@@ -230,9 +222,26 @@ begin
 		product <= { sum_8, sum_7_2[16:0], sum_5_4[6:0], sum_4_5[9:0], sum_2_7[6:0],
 					sum_1_8[9:0], sum_0_9[6:0], product_a_10[16:0] };
 		product_1 <= product << product_shift;
-		product_2 <= product_1; product_3 <= product_2;
-		product_4 <= set_mantissa_zero ? 56'b0 : { 1'b0, product_3[105:52] , |product_3[51:0]};
+		product_2 <= product_1[105:53]; product_3 <= product_2;
+		product_4 <= set_mantissa_zero ? 54'b0 : { 1'b0, product_3};
 		product_shift <= !sum_8[30]; 
+		round_nearest_mode <= rm_15 == 2'b00;
+		round_posinf_mode <= rm_15 == 2'b10;
+		round_neginf_mode <= rm_15 == 2'b11;
+		round_nearest_trigger <= product_1[52];
+		round_nearest_exception <= !(|product_1[51:0]) & (product_1[53] == 0);
+		round_nearest_enable <= round_nearest_mode & round_nearest_trigger & !round_nearest_exception;
+		round_posinf_trigger <= |product_1[52:0] & !sign_15;
+		round_posinf_enable <= round_posinf_mode & round_posinf_trigger;
+		round_neginf_trigger <= |product_1[52:0] & sign_15;
+		round_neginf_enable <= round_neginf_mode & round_neginf_trigger;
+		round_enable <= round_posinf_enable | round_neginf_enable | round_nearest_enable;
+		product_5 <= round_enable & !set_mz_1 ? product_4 + 1 : product_4;
+		product_overflow <= product_5[53];
+		product_6 <= product_5;
+		product_7 <= product_overflow ? product_6 >> 1 : product_6;
+		exponent_7 <= exponent_6; exponent_8 <= exponent_7;
+		exponent_9 <= product_overflow ? exponent_8 + 1 : exponent_8;  
 	end
 end
 
@@ -245,8 +254,8 @@ begin
 	end
 	else if (enable) begin
 		ready <= count_ready;
-		count_ready_0 <= count == 15;
-		count_ready <= count == 16;
+		count_ready_0 <= count == 18;
+		count_ready <= count == 19;
 	end
 end
 
